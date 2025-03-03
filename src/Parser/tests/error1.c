6ba9fa65378ca7021838c4f1e482cfc65cbc7f4a
0graph <50416380+0graph@users.noreply.github.com>
2025-03-03 12:59:07 -0500

    added some more simple error recovery

--- src/Parser/tests/error1.c
+++ src/Parser/tests/error1.c
@@ -1,6 +1,7 @@
 /*
 Error 1:    Invalid String in Declaration List
-            int x has missing semicolon
+Error 2:    Invalid additive expression
+Error 3:    Invalid value in factor parentheses           
 */
 
 int x
@@ -8,5 +9,6 @@ int x
 int func(int t)
 {
     int x;
-    x = 0;
+    x = 0++5;
+    x = (t++);
 }
