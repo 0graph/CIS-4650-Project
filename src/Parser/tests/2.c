f5c98227e82cb94923bc8a126413b3e9de77f1be
0graph <50416380+0graph@users.noreply.github.com>
2025-03-03 19:29:37 -0500

    small chnage to 2.c

--- src/Parser/tests/2.c
+++ src/Parser/tests/2.c
@@ -1,14 +1,12 @@
 /*
-Error 1:    Invalid String in Declaration List
-Error 2:    Invalid additive expression
-Error 3:    Invalid value in factor parentheses           
+Error 1:    Invalid character in expression
+Error 2:    Invalid assignment in expression           
 */
 
-int x
 
 int func(int t)
 {
     int x;
     x = 0++5;
-    x = (t++);
+    x = (t > a = 5);
 }
