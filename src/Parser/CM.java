a418d4ffc0834b5d5f90fcdb86ca87f0ec910517
0graph <50416380+0graph@users.noreply.github.com>
2025-03-11 13:46:47 -0400

    added some makefile / running stuff for main compiler file

--- src/Parser/CM.java
+++ src/Parser/CM.java
@@ -10,19 +10,25 @@ import Ast.*;
 
 public class CM {
 
-    static public void main(String[] args) {
+    static public void main(String args[]) {
+        // no args
         if(args.length() < 1){
-            System.out.println("No Args Specified. Use -a or -s");
+            System.out.println("No Args Specified. Use -a <filepath> or -s <filepath>");
+            return;
+        }
+        //1 arg
+        if(args.length() < 2){
+            System.out.println("No File Specified. Use -a <filepath> or -s <filepath>");
             return;
         }
 
         if(args[0].equals("-a")) {
             System.out.println("Creating an Abstract Syntax Tree.");
-            doAstParse();
+            doAstParse(args[1]);
         }
         else if (args[0].equals("-s")) {
             System.out.println("Creating a Symbol Table");
-            doSymbolParse();
+            doSymbolParse(args[1]);
         } else  {
             System.out.println("Invalid Args Specified. Use -a or -s");
             return;
@@ -30,13 +36,25 @@ public class CM {
 
     }
 
-    static void doSymbolParse() {
+    static void doSymbolParse(String file) {
+        try {
+            Parser p = new Parser(new Lexer(new FileReader(file)));
+            Ast result = (Ast) p.parse().value;
 
+            AstVisitor visitor = new SemanticAnalyser();
+
+            System.out.println("Symbol Table: ");
+
+            result.accept(visitor, 0);
+        } catch (Exception e) {
+            /* do cleanup here -- possibly rethrow e */
+            e.printStackTrace();
+        }
     }
 
-    static void doAstParse() {
+    static void doAstParse(String file) {
         try {
-            Parser p = new Parser(new Lexer(new FileReader(argv[0])));
+            Parser p = new Parser(new Lexer(new FileReader(file)));
             Ast result = (Ast) p.parse().value;
 
             AstVisitor visitor = new DisplayTree();
