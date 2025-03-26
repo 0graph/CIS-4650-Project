e9775cd256c5d9d74e730c86e5eb68a9fe5bbb3b
marcosFIF <marcosc@fillitforward.com>
2025-03-26 17:53:57 -0400

    removed lazly lua to the project since it'sonly local to me

--- src/Parser/.lazy.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"mfussenegger/nvim-jdtls",
-	opts = {
-		root_dir = function(filename)
-			return require("lspconfig").util.root_pattern("makefile")(filename) or vim.fn.getcwd()
-			-- return vim.fs.find({ "" }, { path = filename, upward = true })[1]
-		end,
-	},
-}
